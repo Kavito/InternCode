af56fb38e5dcdc6dbd60900c33ec3d2828351fdf
Ichiorochi <rudiodder@live.dk>
2014-09-08 10:34:21 +0200

    Added Classes

---
+++ src/Model/School.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+
+/**
+ *
+ * @author Rudi
+ */
+public class School {
+ 
+    int authority;
+    String userName;
+    String userPassword;
+    String name;
+    String address;
+    int phone;
+    String email;
+    String linesofEducation;
+    int totalNumberOfStudents;
+    
+}
