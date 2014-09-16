664a91c87240370470d5f3b44386230470de0806
Kavito <kavitokasper@gmail.com>
2014-09-16 12:10:38 +0200

    Mere Dao

--- src/DAO/StudentDAO.java
+++ src/DAO/StudentDAO.java
@@ -6,10 +6,31 @@
 
 package DAO;
 
+import Entity.Student;
+import java.util.Date;
+
 /**
  *
  * @author Kavito
  */
 public class StudentDAO {
     
+    private StudentDAO()
+    {
+        
+    }
+    
+    public static int insertStudent(Student student)
+    {
+        String studentName = student.getStudentName();
+        String studentAge = student.getStudentAge();
+        String studentAddress = student.getStudentAddress();
+        String studentPhone = student.getStudentPhone();
+        String studentEmail = student.getStudentEmail();
+        String studentNationality = student.getStudentNationality();
+        String studentLanguage = student.getStudentLanguage();
+        Date studentInternDate = student.getStudentInternDate();
+        Date studentExamDate = student.getStudentExamDate();
+    }
 }
+
