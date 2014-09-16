a3aaca627358d8313e06655235a9e77ea8339411
Ichiorochi <rudiodder@live.dk>
2014-09-16 14:51:11 +0200

    Working on GUI

---
+++ src/Login.java
@@ -0,0 +1,38 @@
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rudi
+ */
+public class Login extends JFrame{
+    
+    private JLabel labelUsername;
+    private JLabel labelPassword;
+    
+    public Login(){
+        super("Login");
+        setLayout(new FlowLayout());
+        labelUsername = new JLabel("Username");
+        labelUsername.setToolTipText("Please Input your Username below here. Be aware that it is NOT your email!");
+        labelPassword = new JLabel("Password");
+        labelPassword.setToolTipText("Please Input your Password below here. The Password is not readiable for the concernof prying eyes. ");
+        
+        add(labelUsername);
+        add(labelPassword);
+    }
+    
+}
